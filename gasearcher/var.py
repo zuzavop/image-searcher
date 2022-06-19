48fa2f2ea862fee5f69405378111197a65e8986b
zuzavop <82380813+zuzavop@users.noreply.github.com>
2022-06-19 23:25:44 +0200

    update structure

--- gasearcher/var.py
+++ gasearcher/var.py
@@ -2,20 +2,11 @@ import os
 import clip
 import numpy as np
 import torch as torch
-from torchvision.models.resnet import resnet50
-
-path = ""
-
-shown = 96
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-model_resnet = resnet50(pretrained=True)
-model_resnet = model_resnet.eval()
-
 clip_data = []
-resnet_data = []
 
 
 def get_clip_data():
