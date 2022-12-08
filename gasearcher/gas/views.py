efa5cfdec9e921518400fa41c1a64097ea46288e
vopalkoz <zuzana.vop@gmail.com>
2022-12-08 11:50:16 +0100

    update loading

--- gasearcher/gas/views.py
+++ gasearcher/gas/views.py
@@ -1,5 +1,4 @@
 import secrets
-import sys
 from collections import Counter
 
 import clip
@@ -9,15 +8,14 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template import loader
 
-from gas.models import device, model, clip_data, path_data, finding, class_data, classes, last_search, same_video, \
+from gas.models import device, model, clip_data, path_log, finding, class_data, classes, last_search, same_video, \
     showing, class_pr
 
 
 def get_data_from_clip_text_search(query, session, found):
-    # get features of text query
-    text = clip.tokenize([query]).to(device)
+    # get normalize features of text query
     with torch.no_grad():
-        text_features = model.encode_text(text)
+        text_features = model.encode_text(clip.tokenize([query]).to(device))
     text_features /= np.linalg.norm(text_features)
 
     # get distance of vectors
@@ -28,18 +26,14 @@ def get_data_from_clip_text_search(query, session, found):
     last_search[session] = scores
     new_scores = list(np.argsort(new_scores))
 
-    # write down log
-    old_stdout = sys.stdout
-    log_file = open(path_data + "message.csv", "a")
-    sys.stdout = log_file
     # if searching image is present in context (surrounding of image) of any image in shown result same is equal 1
     same = 1 if len(list(set(new_scores[:showing]) & set(same_video[finding[found]]))) > 0 else 0
-    print(query + ';' + str(finding[found]) + ';' + session + ';' + str(new_scores.index(finding[found]) + 1) + ';'
-          + str(same))
-    sys.stdout = old_stdout
-    log_file.close()
+    # write down log
+    with open(path_log, "a") as log:
+        log.write(query + ';' + str(finding[found]) + ';' + session + ';' + str(
+            new_scores.index(finding[found]) + 1) + ';' + str(same) + '\n')
 
-    return new_scores
+    return new_scores[:showing]
 
 
 def get_data_from_clip_image_search(image_query):
@@ -49,7 +43,7 @@ def get_data_from_clip_image_search(image_query):
 
     scores = list(np.argsort(np.concatenate([1 - (torch.cat(clip_data) @ image_query)], axis=None)))
 
-    return scores
+    return scores[:showing]
 
 
 def search(request):
@@ -59,18 +53,18 @@ def search(request):
     found = int(request.COOKIES.get('index')) if request.COOKIES.get('index') is not None else 0
     if found >= len(finding):  # control of end
         return redirect('/end')
-    data = [i for i in range(1, showing + 1)]
+    data = np.arange(1, showing + 1)
 
     if request.GET.get('query'):
-        data = get_data_from_clip_text_search(request.GET['query'], request.session['session_id'], found)[:showing]
+        data = get_data_from_clip_text_search(request.GET['query'], request.session['session_id'], found)
     else:
         # reset save search if user use any other method than text search
         last_search[request.session['session_id']] = np.zeros(len(clip_data))
         if request.GET.get('id'):
-            data = get_data_from_clip_image_search(request.GET['id'])[:showing]
+            data = get_data_from_clip_image_search(request.GET['id'])
 
     # get classes of current shown result
-    data_to_display = {str(i): ([] if i not in class_data else [a for a in class_data[i]]) for i in data}
+    data_to_display = {str(i): ([] if i not in class_data else class_data[i]) for i in data}
     # get top classes contains in result
     top_classes = [word for word, word_count in
                    Counter(np.concatenate([a for a in data_to_display.values()], axis=None)).most_common(5) if
--- gasearcher/static/js/config.js
+++ gasearcher/static/js/config.js
@@ -3,11 +3,11 @@ const config = {
     lines: 5,
     show_last_query: true,
     listing_context: false,
-    photos_address: '../static/data/photos/',
+    photos_address: '../static/data/sea_photos/',
     photos_on_line: 12,
     context_shift: 3,
-    size_dataset: 20000,
+    size_dataset: 22036, // 20000 for v3c and 22036 for sea dataset
     connection: ', ',
     showing_photos: 60,
-    perc_grow: 9, // for sea 1.4 and for v3c 9 - for scaling percentage of occurrence of classes
+    perc_grow: 1.4, // for sea dataset 1.4 and for v3c 9 - for scaling percentage of occurrence of classes
 };
