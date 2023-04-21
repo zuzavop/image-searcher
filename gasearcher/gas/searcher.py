fb7b14df61abdd4d43566d67aaa15a4ef9feca0a
vopalkoz <zuzana.vop@gmail.com>
2023-04-21 16:44:18 +0200

    add comments

--- gasearcher/gas/searcher.py
+++ gasearcher/gas/searcher.py
@@ -5,24 +5,29 @@ import torch
 
 class Searcher:
     """
+    The Searcher class is responsible for searching through a given set of images using text or image queries.
+    It utilizes the CLIP (Contrastive Language-Image Pre-training) model to encode text queries to n-dimensional space.
 
     Attributes:
-        clip_data (list):
-        combination (bool):
-        logger (Logger):
-        showing (int):
-        last_search (dict):
-        device:
-        model:
+        clip_data (list): A list of feature vectors representing the images.
+        combination (bool): A boolean flag indicating whether to combine the scores of the current and previous search
+            queries. If True, the last search scores are added to the current scores.
+            If False, only the current scores are used.
+        logger (Logger): A Logger instance for logging search queries and results.
+        showing (int): The number of top search results which are display.
+        last_search (dict): A dictionary to store the vectors of the last text search for each session.
+        device: A string indicating whether to use CPU or GPU for running the CLIP model.
+        model: The pre-trained CLIP model.
     """
 
     def __init__(self, clip_data, combination, logger, showing):
         """
         Args:
-            clip_data (list):
-            combination (bool):
-            logger (Logger):
-            showing (int):
+            clip_data (list): A list of feature vectors representing the images.
+            combination (bool): A boolean flag indicating whether to combine the scores of the current and previous
+                search queries.
+            logger (Logger): A Logger instance for logging search queries and results.
+            showing (int): The number of top search results which are display.
         """
         self.clip_data = clip_data
         self.combination = combination
@@ -35,7 +40,8 @@ class Searcher:
 
     def result_score(self, features):
         """
-        Calculate the similarity distance of the query feature vector to the CLIP data (normalize feature vectors of images).
+        Calculate the similarity distance of the query feature vector to the CLIP data
+        (normalize feature vectors of images).
 
         Args:
             features (numpy.ndarray): A 2D array representing the normalize feature vector of the query.
@@ -102,6 +108,6 @@ class Searcher:
         Reset scores of last search for user of given session.
 
         Args:
-            session (str): The session.
+            session (str): The unique session ID of the user.
         """
         self.last_search[session] = np.zeros(len(self.clip_data))
